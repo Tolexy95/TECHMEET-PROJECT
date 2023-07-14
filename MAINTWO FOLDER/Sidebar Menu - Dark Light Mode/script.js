af5859c04d840e054a16074dc9c83be2ce76a63d
Tolexy95 <toyin95.olubayo@gmail.com>
2023-07-14 10:35:39 +0100

    push

--- MAINTWO FOLDER/Sidebar Menu - Dark Light Mode/script.js
+++ MAINTWO FOLDER/Sidebar Menu - Dark Light Mode/script.js
@@ -25,8 +25,6 @@ modeSwitch.addEventListener("click" , () =>{
     }
 });
 
-
-
 // Function to retrieve the user credentials from local storage
 function getUserCredentials() {
     return localStorage.getItem("userName");
@@ -49,5 +47,43 @@ function getUserCredentials() {
   // Call the loadUserCredentials function when the page loads
   window.addEventListener("load", loadUserCredentials);
   
- 
+  // Function to handle user search
+  async function handleUserSearch(event) {
+    if (event.key === "Enter") {
+      const searchInput = document.querySelector('.search-box input');
+      const username = searchInput.value.trim();
+  
+      if (username !== '') {
+        try {
+          const userProfileEndpoint = `https://techmeetappwebapi.onrender.com/api/Users/${username}`;
+  
+          // Make API call to check if the user profile exists
+          const response = await fetch(userProfileEndpoint);
+  
+          if (response.ok) {
+            // User profile exists, redirect to the profile page
+            window.location.href = `/PROFILE FOLDER/profile.html?username=${username}`;
+          } else {
+            // User profile does not exist, display an error message or handle accordingly
+            alert('User profile not found');
+          }
+        } catch (error) {
+          console.error('Error:', error);
+        }
+      }
+    }
+  }
+  
+  // Add event listener to the search input for keyup event
+  const searchInput = document.querySelector('.search-box input');
+  searchInput.addEventListener('keyup', handleUserSearch);
+  
+
+  // Alternatively, you can listen to form submission event
+  // const searchForm = document.querySelector('.search-box');
+  // searchForm.addEventListener('submit', (event) => {
+  //   event.preventDefault();
+  //   handleUserSearch();
+  // });
+  
  
