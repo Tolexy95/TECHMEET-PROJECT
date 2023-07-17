db2628717a406acc56b67b0af03553d09a69d449
Tolexy95 <toyin95.olubayo@gmail.com>
2023-07-17 09:41:17 +0100

    push

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const lbs = document.querySelectorAll(".lb");
 const ul = document.querySelector("ul");
 const lineDash = document.querySelector(".line-dash");
 
-
 var dashOrigin = -35; //pixels
 var selectedLi = -35; //pixels
 var speed = 500; //move this many pixels in one second.
@@ -12,25 +11,25 @@ var time = 0;
 
 // initial animation and class for HOME
 TweenLite.to(lbs[0], 0.6, {
-          y: -43,
-          ease: Bounce.easeOut,
-          delay: 1
-        });
+  y: -43,
+  ease: Bounce.easeOut,
+  delay: 1,
+});
 
 lis[0].classList.add("active");
 
 //push all the bottom lines down.
 function pushDownLb() {
-  for(let k = 0; k < lbs.length; ++k)
+  for (let k = 0; k < lbs.length; ++k)
     TweenLite.to(lbs[k], 0.5, {
-          y: 0,
-          ease:  Power3.easeOut
-        });
+      y: 0,
+      ease: Power3.easeOut,
+    });
 }
 
 ul.addEventListener(
   "mouseleave",
-  function(e) {
+  function (e) {
     // to avoid a bug in chrome that sometimes triggers mouseleave on click
     // and the relatedTarget comes up null
     if (e.relatedTarget) {
@@ -41,7 +40,7 @@ ul.addEventListener(
         // overlaping tweens would give a zero time
         TweenLite.to(lineDash, time, {
           strokeDashoffset: selectedLi,
-          ease: Bounce.easeOut
+          ease: Bounce.easeOut,
         });
       } //if
     } //if
@@ -50,73 +49,88 @@ ul.addEventListener(
 );
 
 for (let i = 0; i < 4; ++i) {
-  lis[i].addEventListener("mouseover", function() {
+  lis[i].addEventListener("mouseover", function () {
     distance = Math.abs(-250 * i - 35 - dashOrigin);
     time = distance / speed;
     dashOrigin = -250 * i - 35;
     if (time) {
       TweenLite.to(lineDash, time, {
         strokeDashoffset: -250 * i - 35,
-        ease: Bounce.easeOut
+        ease: Bounce.easeOut,
       });
     } //if
   });
 
-  lis[i].addEventListener("click", function() {
+  lis[i].addEventListener("click", function () {
     selectedLi = -250 * i - 35;
     pushDownLb();
     let current = document.getElementsByClassName("active");
     current[0].classList.remove("active");
     lis[i].classList.add("active");
     TweenLite.to(lbs[i], 0.5, {
-          y: -43,
-          ease: Bounce.easeOut
-        });
+      y: -43,
+      ease: Bounce.easeOut,
+    });
   });
 }
 // Setting up the Variables
 var bars = document.getElementById("nav-action");
 var nav = document.getElementById("nav");
 
-
 //setting up the listener
 bars.addEventListener("click", barClicked, false);
 
-
 //setting up the clicked Effect
 function barClicked() {
-  bars.classList.toggle('active');
-  nav.classList.toggle('visible');
+  bars.classList.toggle("active");
+  nav.classList.toggle("visible");
 }
 
 function displayWordBoxes() {
-    var container = document.querySelector('.writeupContainer');
-    var wordBoxes = document.getElementsByClassName("hireTalent");
-    var currentIndex = 0;
-  
-    function showNextWordBox() {
-      // Hide all WordBoxes
-      for (var i = 0; i < wordBoxes.length; i++) {
-        wordBoxes[i].style.display = "none";
-      }
-  
-      // Display the current WordBox
-      wordBoxes[currentIndex].style.display = "block";
-  
-      // Increment the index to move to the next WordBox
-      currentIndex = (currentIndex + 1) % wordBoxes.length;
-  
-      // Call the function recursively after 8 seconds (8000 milliseconds)
-      setTimeout(showNextWordBox, 8000);
+  var container = document.querySelector(".writeupContainer");
+  var wordBoxes = document.getElementsByClassName("hireTalent");
+  var currentIndex = 0;
+
+  function showNextWordBox() {
+    // Hide all WordBoxes
+    for (var i = 0; i < wordBoxes.length; i++) {
+      wordBoxes[i].style.display = "none";
     }
-  
-    // Delay the initial call of the function by 8 seconds (8000 milliseconds)
+
+    // Display the current WordBox
+    wordBoxes[currentIndex].style.display = "block";
+
+    // Increment the index to move to the next WordBox
+    currentIndex = (currentIndex + 1) % wordBoxes.length;
+
+    // Call the function recursively after 8 seconds (8000 milliseconds)
     setTimeout(showNextWordBox, 8000);
   }
-  
-  // Call the function once the page has finished loading
-  window.addEventListener("load", function () {
-    displayWordBoxes();
-  });
-  
+
+  // Delay the initial call of the function by 8 seconds (8000 milliseconds)
+  setTimeout(showNextWordBox, 8000);
+}
+
+// Call the function once the page has finished loading
+window.addEventListener("load", function () {
+  displayWordBoxes();
+});
+
 export const signUpBtn = document.querySelector(".sign-up-btn");
+
+// hide services section at a certain height
+window.addEventListener("scroll", function () {
+  var myDiv = document.getElementById("services");
+  var scrollHeight =
+    window.pageYOffset ||
+    document.documentElement.scrollTop ||
+    document.body.scrollTop ||
+    0;
+
+  if (scrollHeight >= 600) {
+    // Change 300 to the desired height at which the div should disappear
+    myDiv.style.display = "none";
+  } else {
+    myDiv.style.display = "block";
+  }
+});
